153ae9446ef6bc057c4c7a60fb0592b7882574ff
Kevin Chung <kchung@nyu.edu>
2017-08-20 02:58:30 -0400

    Updating to use Flask templates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, redirect, url_for, escape, request
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 import os
 
 app = Flask(__name__)
@@ -7,41 +7,30 @@ def hostname_lookup(hostname):
     return os.popen('dig +short {}'.format(hostname)).read()
 
 def ping_ip(ip_address):
-    return os.popen('ping -c 4 -i .1 {}'.format(ip_address)).read() 
+    return os.popen('ping -c 4 -i .1 {}'.format(ip_address)).read()
 
 @app.route('/hostname', methods=['GET'])
 def handle_hostname():
     hostname = request.args.get('hostname', None)
-    if not hostname:
-        return "No hostname provided."
-
-    ip_address = hostname_lookup(hostname)
-    return "IP address: " + ip_address
+    if hostname is None:
+        result = "No hostname provided."
+    else:
+        ip_address = hostname_lookup(hostname)
+        result = "IP address: " + ip_address
+    return render_template('results.html', result=result)
 
 @app.route('/ping', methods=['GET'])
 def handle_ping():
     ip_address = request.args.get('ip', None)
-    if not ip_address:
-        return "No IP address provided."
-
-    ping_response = ping_ip(ip_address)
-    return '<pre>'+ping_response+'</pre>'
+    if ip_address is None:
+        result = "No IP address provided."
+    else:
+        ping_response = ping_ip(ip_address)
+        result = ping_response
+    return render_template('results.html', result=result)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    template = open('router_index.html').read()
-    return template.replace('{html}', '''
-        <h1>Your Basic Router's Network Tools Page</h1>
-        <h2>Hostname to IP address</h2>
-        <form action="/hostname">
-            <p><input type=text name=hostname>
-            <p><input type=submit value=lookup!>
-        </form>
-        <h2>Ping IP address</h2>
-        <form action="/ping">
-            <p><input type=text name=ip>
-            <p><input type=submit value=ping!>
-        </form>
-    ''')
+    return render_template('index.html')
 
